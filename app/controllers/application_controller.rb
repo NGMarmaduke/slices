a43793061f6df41e0db6b13a701d283ced787db9
Nick Maher <nim3@aber.ac.uk>
2015-02-20 15:22:59 +0000

    redirect to www domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :add_www_subdomain
+
+  private
+  def add_www_subdomain
+    unless /^www/.match(request.host)
+      redirect_to("#{request.protocol}fitzdares.com#{request.request_uri}",
+                  :status => 301)
+    end
+  end
 end
 
